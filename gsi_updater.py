5aa14a33376412d6274c13c34ec242418d3ef5ce
nkwi <nrmk316@gmail.com>
2024-08-24 22:13:59 +0900

    定数の持ち方を変更

--- gsi_updater.py
+++ gsi_updater.py
@@ -3,13 +3,12 @@ import sys
 import glob
 import pathlib
 import subprocess
-from typing import Literal
 
 
-IMG = Literal[".img"]
-DOT_XZ = Literal[".xz"]
-IMG_XZ = Literal["img", "xz"]
-GSI_TYPE = Literal["aosp", "arm64", "ab", "gapps"]
+IMG: str = ".img"
+DOT_XZ: str = ".xz"
+IMG_XZ: list[str, str] = ["img", "xz"]
+GSI_TYPE: list[str, str] = ["aosp", "arm64", "ab", "gapps"]
 
 
 def search_gsi_img() -> tuple[bool, list[str, str]]:
@@ -29,15 +28,16 @@ def search_gsi_img() -> tuple[bool, list[str, str]]:
 def main():
     ret, gsi_imgs = search_gsi_img()
     if ret and gsi_imgs is not None:
-        for gsi_img in gsi_imgs:
-            if gsi_img.split(".")[-2] == IMG_XZ:
+        for gsi_img in reversed(gsi_imgs):
+
+            if gsi_img.split(".")[-2:] == IMG_XZ:
                 subprocess.run(["unxz", f"{gsi_img}"], shell=True)
 
-            if gsi_img.split("-")[:4] == GSI_TYPE:
-                subprocess.run(["adb", "reboot", "bootloader"], shell=True)
-                subprocess.run(["fastboot", "reboot", "fastboot"], shell=True)
-                subprocess.run(["fastboot flash", "system", f"{gsi_img}"])
-                subprocess.run(["fastboot", "reboot"], shell=True)
+            # if gsi_img.split("/")[-1].split("-")[:4] == GSI_TYPE:
+            #     subprocess.run(["adb", "reboot", "bootloader"], shell=True)
+            #     subprocess.run(["fastboot", "reboot", "fastboot"], shell=True)
+            #     subprocess.run(["fastboot flash", "system", f"{gsi_img}"])
+            #     subprocess.run(["fastboot", "reboot"], shell=True)
 
 
 if __name__ == "__main__":
